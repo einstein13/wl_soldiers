efc82ad5b6301463dc144b42d164d304908edd82
einstein13 <kartezjusz2009@tlen.pl>
2016-02-20 14:07:34 +0100

    Initial files

---
+++ soldiers.py
@@ -0,0 +1,9 @@
+class soldier():
+    health = 0.0
+    attack_min = 0.0
+    attack_max = 0.0
+    evade = 0.0
+    defense = 0.0
+    
+    def set_stats(tribe="Barbarians", attack_level=0, defense_level=0, evade_level=0, health_level=0):
+        return 0
