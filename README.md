660d5e4bb3cf3c60dac2898c0c8e5a77d3bb25bd
einstein13 <kartezjusz2009@tlen.pl>
2016-02-21 18:04:38 +0100

    Readme file created

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # wl_soldiers
-Widelands soldiers fight simulation
+Widelands soldiers fight simulation. In the Widelands' world the fitghts are pseudo-random fights between two soldiers. The fights are always to death. This simulation counts how many soldiers survive the fitght. The number of fights is limited, but very high. This pseudo-random calculations try to find probability of win in that situation.
+
+The model is described on Widelands [wiki page](https://wl.widelands.org/wiki/SoldierLevels/).
+
+Widelands project page is available on the official site: [http://wl.widelands.org](http://wl.widelands.org).
+
+
+
+
--- execute.py
+++ execute.py
@@ -10,18 +10,19 @@ bar10 = {'name':'bar_10', 'tribe':'barbarians', 'attack':5, 'defense':0, 'evade'
 
 #Empire
 emp0 = {'name':'emp_00', 'tribe':'empire', 'attack':0, 'defense':0, 'evade':0, 'health':0} #not promoted soldier
-emp2 = {'name':'emp_00', 'tribe':'empire', 'attack':0, 'defense':0, 'evade':2, 'health':0} #promoted only for food
-emp7 = {'name':'emp_00', 'tribe':'empire', 'attack':2, 'defense':0, 'evade':2, 'health':3} #promoted only for iron
+emp2 = {'name':'emp_02', 'tribe':'empire', 'attack':0, 'defense':0, 'evade':2, 'health':0} #promoted only for food
+emp7 = {'name':'emp_07', 'tribe':'empire', 'attack':2, 'defense':0, 'evade':2, 'health':3} #promoted only for iron
 emp10 = {'name':'emp_10', 'tribe':'empire', 'attack':4, 'defense':0, 'evade':2, 'health':4} #fully promoted soldier
 
 #Atlanteans
 atl0 = {'name':'atl_00', 'tribe':'atlanteans', 'attack':0, 'defense':0, 'evade':0, 'health':0} #not promoted soldier
-atl2 = {'name':'atl_00', 'tribe':'atlanteans', 'attack':0, 'defense':0, 'evade':2, 'health':0} #promoted only for food
-atl7 = {'name':'atl_00', 'tribe':'atlanteans', 'attack':2, 'defense':1, 'evade':2, 'health':0} #promoted only for iron
+atl2 = {'name':'atl_02', 'tribe':'atlanteans', 'attack':0, 'defense':0, 'evade':2, 'health':0} #promoted only for food
+atl7 = {'name':'atl_07', 'tribe':'atlanteans', 'attack':2, 'defense':1, 'evade':2, 'health':0} #promoted only for iron
 atl10 = {'name':'atl_10', 'tribe':'atlanteans', 'attack':4, 'defense':2, 'evade':2, 'health':1} #fully promoted soldier
 
-list_to_test = [bar10, emp10, atl10]
-number_of_tests = 10000
+list_to_test = [bar0, bar2, bar7, bar10, emp0, emp2, emp7, emp10, atl0, atl2, atl7, atl10]
+#list_to_test = [bar10, emp10, atl10]
+number_of_tests = 10**6
 
 stats = simulation.statistics()
 stats.add_soldiers_definitions(list_to_test)
