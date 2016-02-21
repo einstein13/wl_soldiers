78f5bc0cec7c4c8806038a63e02878333f0e5369
einstein13 <kartezjusz2009@tlen.pl>
2016-02-21 03:11:48 +0100

    main simulation completed

---
+++ execute.py
@@ -0,0 +1,29 @@
+import simulation
+
+#example soldiers
+
+#Barbarians
+bar0 = {'name':'bar_00', 'tribe':'barbarians', 'attack':0, 'defense':0, 'evade':0, 'health':0} #not promoted soldier
+bar2 = {'name':'bar_02', 'tribe':'barbarians', 'attack':0, 'defense':0, 'evade':2, 'health':0} #promoted only for food
+bar7 = {'name':'bar_07', 'tribe':'barbarians', 'attack':3, 'defense':0, 'evade':2, 'health':2} #promoted only for iron
+bar10 = {'name':'bar_10', 'tribe':'barbarians', 'attack':5, 'defense':0, 'evade':2, 'health':3} #fully promoted soldier
+
+#Empire
+emp0 = {'name':'emp_00', 'tribe':'empire', 'attack':0, 'defense':0, 'evade':0, 'health':0} #not promoted soldier
+emp2 = {'name':'emp_00', 'tribe':'empire', 'attack':0, 'defense':0, 'evade':2, 'health':0} #promoted only for food
+emp7 = {'name':'emp_00', 'tribe':'empire', 'attack':2, 'defense':0, 'evade':2, 'health':3} #promoted only for iron
+emp10 = {'name':'emp_10', 'tribe':'empire', 'attack':4, 'defense':0, 'evade':2, 'health':4} #fully promoted soldier
+
+#Atlanteans
+atl0 = {'name':'atl_00', 'tribe':'atlanteans', 'attack':0, 'defense':0, 'evade':0, 'health':0} #not promoted soldier
+atl2 = {'name':'atl_00', 'tribe':'atlanteans', 'attack':0, 'defense':0, 'evade':2, 'health':0} #promoted only for food
+atl7 = {'name':'atl_00', 'tribe':'atlanteans', 'attack':2, 'defense':1, 'evade':2, 'health':0} #promoted only for iron
+atl10 = {'name':'atl_10', 'tribe':'atlanteans', 'attack':4, 'defense':2, 'evade':2, 'health':1} #fully promoted soldier
+
+list_to_test = [bar0, emp0, atl0]
+number_of_tests = 10000
+
+stats = simulation.statistics()
+stats.add_soldiers_definitions(list_to_test)
+stats.simulate_all(number_of_tests)
+stats.print_results()
--- simulation.py
+++ simulation.py
@@ -26,5 +26,70 @@ class one_fight():
         self.define_soldiers(soldier1_dict, soldier2_dict)
         result = 0
         while result == 0:
-            result = simulation_step()
+            result = self.simulation_step()
         return result
+
+class main_simulation():
+    soldier1_wins = 0
+    soldier2_wins = 0
+    
+    def simulate(self, soldier1_dict, soldier2_dict, number_of_fights):
+        self.soldier1_wins = 0
+        self.soldier2_wins = 0
+        figth = ""
+        itr = 0
+        while itr < number_of_fights:
+            itr += 1
+            figth = ""
+            figth = one_fight()
+            result = figth.simulate(soldier1_dict, soldier2_dict)
+            if result == 1:
+                self.soldier2_wins += 1
+            elif result == 2:
+                self.soldier1_wins += 1
+        return 1.0 * self.soldier1_wins / number_of_fights
+
+class statistics():
+    result_matrix = []
+    dictionaries_list = []
+    
+    def add_soldiers_definitions(self, dictionary_or_list):
+        if isinstance(dictionary_or_list, dict):
+            self.dictionaries_list.append(dictionary_or_list)
+            return 0
+        for element in dictionary_or_list:
+            self.dictionaries_list.append(element)
+        return 0
+
+    def construct_result_matrix(self):
+        length = len(self.dictionaries_list)
+        self.result_matrix = []
+        for itr1 in range(length):
+            self.result_matrix.append([])
+            for itr2 in range(length):
+                self.result_matrix[itr1].append(0.0)
+        return 0
+
+    def simulate_one(self, soldier1, soldier2, number_of_fights):
+        simulation = main_simulation()
+        dict1 = self.dictionaries_list[soldier1]
+        dict2 = self.dictionaries_list[soldier2]
+        result = 100.0*simulation.simulate(dict1, dict2, number_of_fights)
+        self.result_matrix[soldier1][soldier2] = result
+        return result
+
+    def simulate_all(self, number_of_fights):
+        length = len(self.dictionaries_list)
+        self.construct_result_matrix()
+        for itr1 in range(length):
+            for itr2 in range(length):
+                res = self.simulate_one(itr1, itr2, number_of_fights)
+                print(str(self.dictionaries_list[itr1]['name'])+" vs. "+str(self.dictionaries_list[itr2]['name'])+"\t"+str(res)+"%")
+        return 0
+
+    def print_results(self):
+        length = len(self.dictionaries_list)
+        for itr1 in range(length):
+            for itr2 in range(length):
+                print(str(itr1)+"\t"+str(itr2)+"\t"+str(self.result_matrix[itr1][itr2]))
+        return 0
